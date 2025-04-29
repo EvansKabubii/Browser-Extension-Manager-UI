616c0d19e87fbd7b670ba8178a0e4690a65335ba
Evans Kabubii <kabubiievans@gmail.com>
2025-04-29 22:12:23 +0300

    Hello good sir, yes this was an update. Yes, it was cool. No, I will not elaborate. Yes it was a lot. I think it is the final one too :)

--- DynamicTing.js
+++ DynamicTing.js
@@ -1,13 +1,36 @@
-const themeSwitcher = document.getElementById('ThemeChanger');
-const gridItems = document.querySelectorAll('.GridItem');
-const header = document.getElementById('Search');
+const themeSwitcher = document.getElementById("ThemeChanger");
+const gridItems = document.querySelectorAll(".GridItem");
+const header = document.getElementById("Search");
+const buttons = document.querySelectorAll(".Buttons");
+const heading2 = document.querySelectorAll("h2");
+const heading3 = document.querySelectorAll("h3");
+const removeButton = document.querySelectorAll(".RemoveButton");
+const themeButton = document.getElementById("ThemeChanger");
+const themeIcon = document.getElementById("themeIcon");
 
-themeSwitcher.addEventListener('click', function() {
-  document.body.classList.toggle('light-mode-body');
-  gridItems.forEach(item => {
-    item.classList.toggle('light-modeb');
+themeSwitcher.addEventListener("click", function () {
+  document.body.classList.toggle("light-mode-body");
+  gridItems.forEach((item) => {
+    item.classList.toggle("light-modeb");
   });
-  header.classList.toggle('light-modeb');
-  console.log(header);
+  buttons.forEach((item) => {
+    item.classList.toggle("light-modeb");
+  });
+  heading2.forEach((item) => {
+    item.classList.toggle("light-h2");
+  });
+  heading3.forEach((item) => {
+    item.classList.toggle("light-h2");
+  });
+  removeButton.forEach((item) => {
+    item.classList.toggle("light-modeb");
+  });
+  header.classList.toggle("light-modeb");
+  themeButton.classList.toggle("light-theme");
 
+  if (themeIcon.src.includes("icon-sun.svg")) {
+    themeIcon.src = "Images/icon-moon.svg";
+  } else {
+    themeIcon.src = "Images/icon-sun.svg";
+  }
 });
